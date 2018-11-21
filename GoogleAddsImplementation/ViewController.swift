ee544854739d673a6728951becce21a8c302cc7c
Shubham <ranufrancis@Shubhams-Mac-mini.local>
2018-11-21 11:25:22 +0530

    update 21st Nov

--- GoogleAddsImplementation/ViewController.swift
+++ GoogleAddsImplementation/ViewController.swift
@@ -7,14 +7,56 @@
 //
 
 import UIKit
+import GoogleMobileAds
 
-class ViewController: UIViewController {
+class ViewController: UIViewController  {
 
+   
+    @IBOutlet weak var adveriseView: GADBannerView!
+    
+//    lazy var adBannerView: GADBannerView = {
+////        let adBannerView = GADBannerView(adSize: kGADAdSizeSmartBannerPortrait)
+//                let adBannerView = GADBannerView(adSize: kGADAdSizeBanner)
+//
+//        adBannerView.adUnitID = "ca-app-pub-4535091644785601/3684884013"
+//        adBannerView.delegate = self
+//        adBannerView.rootViewController = self
+//
+//        return adBannerView
+//    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        adveriseView.load(GADRequest())
+        
+        //test ads
+      adveriseView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+       
+        //production ads
+       
+        adveriseView.delegate = self
+        adveriseView.rootViewController = self
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+}
+
+
+extension ViewController : GADBannerViewDelegate {
+    func adViewDidReceiveAd(_ bannerView: GADBannerView) {
+        print("banner Load Successfully")
+        adveriseView.frame = bannerView.frame
+        adveriseView.addSubview(bannerView)
+        self.view.layoutIfNeeded()
+        
+    }
+    
+    func adView(_ bannerView: GADBannerView, didFailToReceiveAdWithError error: GADRequestError) {
+        print("fail to load Ads")
+        print(error.localizedDescription)
+    }
 }
 
---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'GoogleAddsImplementation' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+ pod 'Google-Mobile-Ads-SDK'
+  # Pods for GoogleAddsImplementation
+
+  target 'GoogleAddsImplementationTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'GoogleAddsImplementationUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
