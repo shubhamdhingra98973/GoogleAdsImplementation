78404ffeecf154c27c765d8860b234ef2156713e
Ranu Bhai <ranufrancis@shubhams-mac-mini.local>
2018-11-21 09:13:49 +0530

    Initial Commit

---
+++ GoogleAddsImplementation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoogleAddsImplementation
+//
+//  Created by Shubham on 21/11/18.
+//  Copyright © 2018 Franciscan Ecare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
